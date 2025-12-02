3b2970f67572eca29464b5842287fe0910e2174b
Gabriel Athia <g.athia@nsaglobal.net>
2025-12-02 09:43:38 -0300

    First commit

---
+++ src/main/java/com/gabrielathia/crudspringboottasks/CrudSpringbootTasksApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielathia.crudspringboottasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringbootTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielathia/crudspringboottasks/CrudSpringbootTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielathia.crudspringboottasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
